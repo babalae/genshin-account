f1c21e753fb41618ffdb090b43dbaf675ec2ace4
huiyadanli <huiyadanli@gmail.com>
2020-10-17 14:06:28 +0800

    [+] init

---
+++ GenshinAccount/FormMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace GenshinAccount
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.rtbAcccount = new System.Windows.Forms.RichTextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // btnSave
+            // 
+            resources.ApplyResources(this.btnSave, "btnSave");
+            this.btnSave.Name = "btnSave";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // rtbAcccount
+            // 
+            resources.ApplyResources(this.rtbAcccount, "rtbAcccount");
+            this.rtbAcccount.Name = "rtbAcccount";
+            // 
+            // btnRefresh
+            // 
+            resources.ApplyResources(this.btnRefresh, "btnRefresh");
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // FormMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.rtbAcccount);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox rtbAcccount;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ GenshinAccount/FormMain.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenshinAccount
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            btnRefresh_Click(sender, e);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this, "修改前请先备份自己的账户信息哦！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                Registry.SetValue(@"HKEY_CURRENT_USER\Software\miHoYo\原神", "ACCOUNT_DATA_LIST_FILEPROD_CN_h934817908", Encoding.UTF8.GetBytes(rtbAcccount.Text));
+                MessageBox.Show("修改成功");
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            object value = Registry.GetValue(@"HKEY_CURRENT_USER\Software\miHoYo\原神", "ACCOUNT_DATA_LIST_FILEPROD_CN_h934817908", "");
+            rtbAcccount.Text = Encoding.UTF8.GetString((byte[])value);
+        }
+    }
+}
